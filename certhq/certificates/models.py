ae98dbabc77d0d80acc63b0b156bdf26070c4574
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-02-02 10:34:49 +0000

    Refactor stringify_x509_name

--- certhq/certificates/models.py
+++ certhq/certificates/models.py
@@ -10,23 +10,20 @@ from model_utils.models import TimeStampedModel
 
 def stringify_x509_name(x509_name):
     """Returns a string representation of an X509Name object"""
-    name = ""
-    for rdn in x509_name.get_components()[::-1]:
-        name += rdn[0].decode('utf-8')
-        name += '='
-        name += rdn[1].decode('utf-8')
-        name += ', '
-    name = name[:-2]
-    return name
+    return ', '.join(
+        '{0}={1}'.format(key.decode('utf-8'), val.decode('utf-8'))
+        for key, val in x509_name.get_components()[::-1])
+
 
 
 class CertificateManager(models.Manager):
     def create_certificate(self, pem_data):
         c = crypto.load_certificate(crypto.FILETYPE_PEM, pem_data)
-        x509_name = c.get_subject()
-        subject = stringify_x509_name(x509_name)
+        subject = stringify_x509_name(c.get_subject())
+        issuer = stringify_x509_name(c.get_issuer())
+        cn = c.get_subject().CN
         cert = Certificate(
-            pem_data=pem_data, subject=subject, cn=c.get_subject().CN)
+            pem_data=pem_data, subject=subject, issuer=issuer, cn=cn)
         cert.save()
         return cert
 

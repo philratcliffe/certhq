0bdec5440e152b3d75c3b2fe5c77c63981c925db
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-02-16 09:56:32 +0000

    Display attribute names along with values

--- client/list_certs.py
+++ client/list_certs.py
@@ -1,14 +1,13 @@
-import requests
 import json
 
+import requests
 
 url = 'http://localhost:8000/api/v1/certificates'
 r = requests.get(url)
 list_of_results = json.loads(r.text)
 for result in list_of_results:
-    print(result['id'])
-    print(result['cn'])
-    print(result['subject'])
-    print(result['sha256_fingerprint'])
-    print(result['pem_data'])
-
+    print('id:', result['id'])
+    print('cn:', result['cn'])
+    print('subject:', result['subject'])
+    print('sha256_fingerprint:', result['sha256_fingerprint'])
+    print('pem_data:', result['pem_data'])

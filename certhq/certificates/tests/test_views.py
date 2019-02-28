b05af184e493d4bd766462c6a01be7074af33244
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-02-28 11:32:35 +0000

    Add test for certificate list

--- certhq/certificates/tests/test_views.py
+++ certhq/certificates/tests/test_views.py
@@ -1,6 +1,9 @@
 from django.test import Client
 from django.test import TestCase
+from django.urls import reverse
 from http import HTTPStatus
+
+from certhq.certificates.models import Certificate
 
 class TestErrorConditions(TestCase):
 
@@ -9,4 +12,41 @@ class TestErrorConditions(TestCase):
         client = Client()
         response = client.get(invalid_url)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+
+class TestCertificateListView(TestCase):
+
+    def setUp(self):
+        self.cert_list_url = reverse('certificates')
+        Certificate.objects.create(pem_data=TEST_EXPIRED_RSA_2048_CERT)
+
+    def test_list_one_cert(self):
+        client = Client()
+        response = client.get(self.cert_list_url)
+        print("debug", self.cert_list_url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+
+TEST_EXPIRED_RSA_2048_CERT=b"""
+-----BEGIN CERTIFICATE-----
+MIIC2jCCAkMCAg38MA0GCSqGSIb3DQEBBQUAMIGbMQswCQYDVQQGEwJKUDEOMAwG
+A1UECBMFVG9reW8xEDAOBgNVBAcTB0NodW8ta3UxETAPBgNVBAoTCEZyYW5rNERE
+MRgwFgYDVQQLEw9XZWJDZXJ0IFN1cHBvcnQxGDAWBgNVBAMTD0ZyYW5rNEREIFdl
+YiBDQTEjMCEGCSqGSIb3DQEJARYUc3VwcG9ydEBmcmFuazRkZC5jb20wHhcNMTIw
+ODIyMDUyNzQxWhcNMTcwODIxMDUyNzQxWjBKMQswCQYDVQQGEwJKUDEOMAwGA1UE
+CAwFVG9reW8xETAPBgNVBAoMCEZyYW5rNEREMRgwFgYDVQQDDA93d3cuZXhhbXBs
+ZS5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC0z9FeMynsC8+u
+dvX+LciZxnh5uRj4C9S6tNeeAlIGCfQYk0zUcNFCoCkTknNQd/YEiawDLNbxBqut
+bMDZ1aarys1a0lYmUeVLCIqvzBkPJTSQsCopQQ9V8WuT252zzNzs68dVGNdCJd5J
+NRQykpwexmnjPPv0mvj7i8XgG379TyW6P+WWV5okeUkXJ9eJS2ouDYdR2SM9BoVW
++FgxDu6BmXhozW5EfsnajFp7HL8kQClI0QOc79yuKl3492rH6bzFsFn2lfwWy9ic
+7cP8EpCTeFp1tFaD+vxBhPZkeTQ1HKx6hQ5zeHIB5ySJJZ7af2W8r4eTGYzbdRW2
+4DDHCPhZAgMBAAEwDQYJKoZIhvcNAQEFBQADgYEAQMv+BFvGdMVzkQaQ3/+2noVz
+/uAKbzpEL8xTcxYyP3lkOeh4FoxiSWqy5pGFALdPONoDuYFpLhjJSZaEwuvjI/Tr
+rGhLV1pRG9frwDFshqD2Vaj4ENBCBh6UpeBop5+285zQ4SI7q4U9oSebUDJiuOx6
++tZ9KynmrbJpTSi0+BM=
+-----END CERTIFICATE-----
+"""
+
 

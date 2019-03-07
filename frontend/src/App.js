0dac6e5651f0ab004e5716bb09bbf4be6af8ca5e
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-03-07 19:01:07 +0000

    Update page title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom'
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import CertificatesList from './CertificatesList'
 import './App.css';
 
@@ -8,7 +8,7 @@ import './App.css';
 const BaseLayout = () => (
     <div className="container-fluid">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Django React Demo</a>
+            <a className="navbar-brand" href="#">CertHQ</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>

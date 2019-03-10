d36100d03810512266f63759b4690123cdf564db
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-03-10 09:52:03 +0000

    Update so works with pagination API

--- client/list_certs.py
+++ client/list_certs.py
@@ -4,8 +4,10 @@ import requests
 
 url = 'http://localhost:8000/api/v1/certificates'
 r = requests.get(url)
-list_of_results = json.loads(r.text)
-for result in list_of_results:
+d = json.loads(r.text)
+results = d['results']
+
+for result in results:
     print('id:', result['id'])
     print('cn:', result['cn'])
     print('subject:', result['subject'])

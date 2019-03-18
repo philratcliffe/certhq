f52da182f909687fbbdd8a64291f9d7cf5227e9a
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-03-18 11:28:20 +0000

    Add cert details view

--- certhq/certificates/views.py
+++ certhq/certificates/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import DetailView, ListView
 from .models import Certificate
 
 app_name = 'certificates'
 
-
 class CertificateList(ListView):
+    model = Certificate
+
+class CertificateDetail(DetailView):
     model = Certificate

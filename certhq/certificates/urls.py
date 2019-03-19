62a80eb167b73a610776cb0946fdc5d717e29156
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-03-19 09:36:22 +0000

    Add path for certificate details

--- certhq/certificates/urls.py
+++ certhq/certificates/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CertificateList
+from .views import CertificateList, CertificateDetail
 
 urlpatterns = [
     path('', CertificateList.as_view(), name='certificates'),
+    path('<int:pk>', CertificateDetail.as_view(), name='certificate_detail'),
 ]

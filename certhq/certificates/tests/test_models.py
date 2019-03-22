b01ea07d4db7b90cef23d80086eecb2059e8ccd1
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-03-22 11:41:31 +0000

    Fix formatting

--- certhq/certificates/tests/test_models.py
+++ certhq/certificates/tests/test_models.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 
 from certhq.certificates.models import Certificate
 
-class CertificateModelTest(TestCase):
 
+class CertificateModelTest(TestCase):
     def setUp(self):
         Certificate.objects.create(pem_data=TEST_EXPIRED_RSA_2048_CERT)
 
@@ -29,13 +29,13 @@ class CertificateModelTest(TestCase):
 
 
 class CertificateModelTestIsExpired(TestCase):
-
     def test_when_not_expired(self):
         Certificate.objects.create(pem_data=TEST_EXPIRED_RSA_2048_CERT)
         cert = Certificate.objects.all()[0]
         self.assertFalse(cert.is_expired())
 
-TEST_EXPIRED_RSA_2048_CERT=b"""
+
+TEST_EXPIRED_RSA_2048_CERT = b"""
 -----BEGIN CERTIFICATE-----
 MIIC2jCCAkMCAg38MA0GCSqGSIb3DQEBBQUAMIGbMQswCQYDVQQGEwJKUDEOMAwG
 A1UECBMFVG9reW8xEDAOBgNVBAcTB0NodW8ta3UxETAPBgNVBAoTCEZyYW5rNERE
@@ -55,4 +55,3 @@ rGhLV1pRG9frwDFshqD2Vaj4ENBCBh6UpeBop5+285zQ4SI7q4U9oSebUDJiuOx6
 +tZ9KynmrbJpTSi0+BM=
 -----END CERTIFICATE-----
 """
-

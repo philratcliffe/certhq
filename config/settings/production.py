da1fc6ff708b0ce083f3299a00d425a1823d7364
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-03-26 14:05:37 +0000

    Change ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['certhq-test.d2zdmfi6sj.eu-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['certhq-stage.eu-west-2.elasticbeanstalk.com']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
